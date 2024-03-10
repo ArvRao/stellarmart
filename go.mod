9a8d6e2f135bd5d50a71e9143bc0d0932703efd5
arvrao <arvindrao.759@gmail.com>
2024-03-11 01:04:42 +0530

    Added supabase connection. supabase client for Go

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.1
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.2
+	github.com/supabase-community/supabase-go v0.0.1
 	go.mongodb.org/mongo-driver v1.14.0
 )
 
@@ -15,6 +16,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/supabase/postgrest-go v0.0.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- internal/app/users/models.go
+++ internal/app/users/models.go
@@ -1,34 +1,50 @@
 package users
 
-import (
-	"time"
-
-	"github.com/ArvRao/ecommerce-app/internal/app/orders"
-	"github.com/ArvRao/ecommerce-app/internal/app/products"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
+import "time"
 
 type User struct {
-	ID              primitive.ObjectID     `json:"_id" bson:"_id"`
-	First_Name      *string                `json:"first_name" validate:"required,min=2,max=30"`
-	Last_Name       *string                `json:"last_name" validate:"required,min=2,max=30"`
-	Password        *string                `json:"password"  validate:"required,min=8"`
-	Email           *string                `json:"email"  validate:"email, required"`
-	Phone           *string                `json:"phone" validate:"required"`
-	Token           *string                `json:"token"`
-	Refresh_Token   *string                `json:"refresh_token"`
-	Created_At      time.Time              `json:"created_at"`
-	Updated_At      time.Time              `json:"updated_at"`
-	User_ID         string                 `json:"user_id"`
-	UserCart        []products.ProductUser `json:"usercart" bson:"usercart"`
-	Address_Details []Address              `json:"address" bson:"address"`
-	Order_Status    []orders.Order         `json:"orders" bson:"orders"`
+	ID           string    `json:"-"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	Password     string    `json:"-"`
+	Email        string    `json:"email"`
+	Phone        string    `json:"phone"`
+	Token        string    `json:"-"`
+	RefreshToken string    `json:"-"`
+	CreatedAt    time.Time `json:"-"`
+	UpdatedAt    time.Time `json:"-"`
+	UserID       string    `json:"-"`
+	Address      Address   `json:"address"`
 }
 
+/* type User struct {
+	ID
+	First_Name
+	Last_Name
+	Password
+	Email
+	Phone
+	Token
+	Refresh_Token
+	Created_At
+	Updated_At
+	User_ID
+	// UserCart        []products.ProductUser
+	Address_Details []Address
+	Order_Status
+} */
+
 type Address struct {
-	Address_id primitive.ObjectID `bson:"_id"`
-	House      *string            `json:"house_name" bson:"house_name"`
-	Street     *string            `json:"street_name" bson:"street_name"`
-	City       *string            `json:"city_name" bson:"city_name"`
-	Pincode    *string            `json:"pincode" bson:"pincode"`
+	ID        string `json:"-"`
+	HouseName string `json:"housename"`
+	City      string `json:"city"`
+	Country   string `json:"country"`
 }
+
+// type Address struct {
+// Address_id primitive.ObjectID `bson:"_id"`
+// House      *string            `json:"house_name" bson:"house_name"`
+// Street     *string            `json:"street_name" bson:"street_name"`
+// City       *string            `json:"city_name" bson:"city_name"`
+// Pincode    *string            `json:"pincode" bson:"pincode"`
+// }
